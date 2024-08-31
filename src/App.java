8991173975ddae3ab218df001aa2dfc28cfefe9a
dev-from-cbd <veryfatwombat@gmail.com>
2024-09-01 08:30:15 +0930

    The Game

---
+++ src/App.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        int boardWidth = 360;
+        int boardHeight = 640;
+
+        JFrame frame = new JFrame("Flappy Bird");
+        frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
---
+++ src/FlappyBird.java
@@ -0,0 +1,7 @@
+import java.awt.*;
+import java.awt.event.*;
+
+
+public class FlappyBird {
+    
+}
