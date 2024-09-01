923918480498d32c63e3a7617f51e24167d8a311
dev-from-cbd <veryfatwombat@gmail.com>
2024-09-02 06:48:05 +0930

    The Game

--- src/App.java
+++ src/App.java
@@ -6,10 +6,15 @@ public class App {
         int boardHeight = 640;
 
         JFrame frame = new JFrame("Flappy Bird");
-        frame.setVisible(true);
+        //frame.setVisible(true);
         frame.setSize(boardWidth, boardHeight);
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        FlappyBird flappyBird = new FlappyBird();
+        frame.add(flappyBird);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
--- src/FlappyBird.java
+++ src/FlappyBird.java
@@ -1,7 +1,35 @@
 import java.awt.*;
 import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.Random;
+import javax.swing.*;
 
+public class FlappyBird extends JPanel {
+    int boardWidth = 360;
+    int boardHeight = 640;
 
-public class FlappyBird {
-    
+    Image backgroundImg;
+    Image birdImg;
+    Image topPipeImg;
+    Image bottomPipeImg;
+
+    FlappyBird() {
+        setPreferredSize(new Dimension(boardWidth, boardHeight));
+        setBackground(Color.BLUE);
+
+        //load images
+        backgroundImg = new ImageIcon(getClass().getResource("./flappybirdbg.png")).getImage();
+        birdImg = new ImageIcon(getClass().getResource("./flappybird.png")).getImage();
+        topPipeImg = new ImageIcon(getClass().getResource("./toppipe.png")).getImage();
+        bottomPipeImg = new ImageIcon(getClass().getResource("./bottompipe.png")).getImage();
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        draw(g);
+    }
+
+    public void draw(Graphics g) {
+        g.drawImage(backgroundImg, 0, 0, boardWidth, boardHeight, null);
+    }
 }
